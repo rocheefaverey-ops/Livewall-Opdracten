6d8d27a20743a58715c16a242ab3bfbc5bf37530
Tom Luijten <tom@livewallgroup.com>
2022-09-29 13:49:53 +0200

    Feature: added typography

--- frontend/components/ui/typography/typography.component.tsx
+++ frontend/components/ui/typography/typography.component.tsx
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
 
 type Props = {
-  type?: string;
+  type?: keyof typeof typeMapping;
   color?: string;
-  variant?: string;
   className?: string;
+  children: ReactNode;
+  [key: string]: unknown;
 };
 
-const typeMapping = {
-  h1: {type:"h1", defaultHeader:"header1"},
-  h2: {type:"h2", defaultHeader:"header2"},
-  h3: {type:"h3", defaultHeader:"header3"},
-  h4: {type:"h4", defaultHeader:"header4"},
-  h5: {type:"h5", defaultHeader:"header5"},
-  h6: {type:"h6", defaultHeader:"header6"},
-  label: {type:"label", defaultHeader:"label"}
+export const typeMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  span: 'span',
+  ul: 'ul',
+  ol: 'ol',
+  p: 'p',
+  small: 'p',
+  label: 'p',
+  'label-big': 'p',
+  'intro-line': 'p',
+  'extra-info': 'p',
+  'call-to-action': 'p'
 };
 
-const Typography: React.FC<Props> = ({children, type, variant = '', color = 'text-primary', className = ''}) => {
-  const CustomTag = type ? typeMapping[type].type : "p";
-  const CustomVariant = type && variant === '' ? typeMapping[type].defaultHeader : variant
-  return <CustomTag className={`${className} ${CustomVariant} ${color}`}>{children}</CustomTag>
-};
+const Typography: React.FC<Props> = ({ children, type = 'p', color = '', className = '', ...props }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const CustomTag = (typeMapping[type] ?? 'p') as any;
 
-// const Typography: React.FC<Props> = ({ children, type, color = 'text-marine01', className }) => {
-//   if (['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'span', 'p', 'blockquote', 'ul'].includes(type)) {
-//     let styling = classNames(color, className);
-//     let CustomTag = type;
-//     if (type === 'h7') {
-//       CustomTag = 'h6';
-//       styling = classNames(color, className, 'h7');
-//     }
-//     return <CustomTag className={styling}>{children}</CustomTag>;
-//   }
-//   if (['label', 'label large'].includes(type)) {
-//     const styling = classNames(type, color, className);
-//     return <div className={styling}>{children}</div>;
-//   }
-//   const styling = classNames(type, color, className);
-//   return <p className={styling}>{children}</p>;
-// };
+  const styling = classNames(
+    { small: type === 'small' },
+    { label: type === 'label' },
+    { 'call-to-action': type === 'call-to-action' },
+    { [`!${color}`]: color },
+    className
+  );
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <CustomTag {...props} className={styling}>
+      {children}
+    </CustomTag>
+  );
+};
 
 export default Typography;
