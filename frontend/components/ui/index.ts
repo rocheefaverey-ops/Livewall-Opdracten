129f6ae0ad0c43f70407b32cf5900de151e4fd4d
glennzegers1 <glennzegers@livewallgroup.com>
2022-10-04 16:23:55 +0200

    feat: Dropdown input

--- frontend/components/ui/index.ts
+++ frontend/components/ui/index.ts
@@ -3,7 +3,8 @@ import Typography from './typography/typography.component';
 import Container from './container/container.component';
 
 // Inputs
-import TextInput from './inputs/text-input/text-input.component';
 import InputHolder from './inputs/components/input-holder/input-holder.component';
+import TextInput from './inputs/text-input/text-input.component';
+import DropdownInput from './inputs/dropdown-input/dropdown-input.component';
 
-export { Typography, Container, TextInput, InputHolder };
+export { Typography, Container, TextInput, InputHolder, DropdownInput };
