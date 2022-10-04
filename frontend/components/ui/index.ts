3440664dc40b701f7e0bc653e04ef7395d8990e3
glennzegers1 <glennzegers@livewallgroup.com>
2022-10-04 17:16:11 +0200

    feat: date input

--- frontend/components/ui/index.ts
+++ frontend/components/ui/index.ts
@@ -5,6 +5,7 @@ import Container from './container/container.component';
 // Inputs
 import InputHolder from './inputs/components/input-holder/input-holder.component';
 import TextInput from './inputs/text-input/text-input.component';
+import DateInput from './inputs/date-input/date-input.component';
 import DropdownInput from './inputs/dropdown-input/dropdown-input.component';
 
-export { Typography, Container, TextInput, InputHolder, DropdownInput };
+export { Typography, Container, TextInput, InputHolder, DropdownInput, DateInput };
