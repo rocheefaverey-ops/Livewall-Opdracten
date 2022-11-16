ae8c6756956d8464f450c61fc8b76a177482a698
Stijn Meevis <stijn@livewallgroup.com>
2022-11-16 12:26:39 +0000

    update: tweak CSP

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,10 +11,10 @@ const ContentSecurityPolicy = `
   frame-src https://consentcdn.cookiebot.com; 
   media-src 'self'; 
   prefetch-src 'self';
-  connect-src 'self' https://www.google-analytics.com; 
+  connect-src 'self' https://www.google-analytics.com https://consentcdn.cookiebot.com; 
   font-src 'self' 'unsafe-inline' data: https://use.typekit.net https://fonts.gstatic.com; 
   img-src 'self' data: blob:; 
-  script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net/en_US/fbevents.js https://consent.cookiebot.com https://consentcdn.cookiebot.com; 
+  script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net/en_US/fbevents.js https://consent.cookiebot.com https://consentcdn.cookiebot.com https://googleads.g.doubleclick.net; 
   style-src 'self' 'unsafe-inline' https://p.typekit.net https://use.typekit.net;
 `;
 
